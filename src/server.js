4bd3be9f61a86cd368d9b4346c082f6c6277c24a
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-04-04 18:30:52 +0700

    fix scripts

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import router from "./routes/index.js";
+import connectMongoDB from "./ultils/connect.js";
+import { PORT } from "./ultils/env.js";
+import { errorHandler, errorHandlerNotFound } from "./ultils/errorHandler.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectMongoDB()
+
+app.use('/', router)
+app.use(errorHandlerNotFound, errorHandler);
+
+app.listen(PORT, () => {
+    console.log("Connecting to port " + PORT);
+})
+
+;
