3db379d31fcde77c6f8db2bc5da74d0a5fc90a4f
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-04-04 17:40:29 +0700

    fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,14 @@ import authRouter from './auth.js';
 import { Router } from 'express';
 const router = Router();
 
+
+router.get('/', (req, res) => {
+    res.send("Home")
+})
 router.use('/movies', movieRouter)
 router.use('/categories', categoryRouter)
 router.use('/genres', genreRouter)
 router.use('/auth', authRouter)
+
 
 export default router
