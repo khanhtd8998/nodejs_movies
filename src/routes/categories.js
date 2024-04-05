bc8f34f37edf665ca7275a465625a2b8dbdbd5e3
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-04-05 11:09:06 +0700

    validation category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import CategoriesController from '../controllers/categories.js';
 import checkPermission from '../middleware/checkPermission.js';
+import validBodyRequest from '../middleware/validRequest.js';
+import categorySchema from '../validation/category.js';
 
 const categoriesController = new CategoriesController();
 const categoryRouter = express.Router();
@@ -9,6 +11,7 @@ categoryRouter.get("/", categoriesController.getAll)
 categoryRouter.get("/:id", categoriesController.getDetail)
 
 // categoryRouter.use(checkPermission)
+categoryRouter.use(validBodyRequest(categorySchema))
 categoryRouter.post("/",  categoriesController.create)
 categoryRouter.put("/:id", categoriesController.update)
 categoryRouter.delete("/:id", categoriesController.remove)
